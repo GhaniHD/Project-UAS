b0e31e302879d909b9503a3617a49361ba7fce72
GhaniHD <120998349+GhaniHD@users.noreply.github.com>
2024-12-30 10:16:30 +0700

    s

--- resep-app-frontend/dev-dist/sw.js
+++ resep-app-frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7h309r2i588"
+    "revision": "0.pou2q51v7og"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
